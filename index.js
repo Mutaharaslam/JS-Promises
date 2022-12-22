088eb1094f084cf13fb41af716df9cd568485732
Mutaharaslam <35948170+Mutaharaslam@users.noreply.github.com>
2022-12-22 23:04:05 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let s  =  new Promise((resolve, reject) => {
+        let a = 1 + 2;
+        if (a == 3){
+            resolve('Promise Resolved')
+        } else{
+            reject('Promise Broken')
+        }
+    })
+    s.then((message) =>{
+        <div>Success {message}</div>
+    }).catch((message) =>{
+        <div>Failed {message}</div>
+    })
